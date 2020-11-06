61d22fb45aa0d358db1a0fcc27f473e6e455e676
unknown <persson.lucas.anton@gmail.com>
2020-11-06 16:18:40 +0100

    no delet

--- routers/athlete.router.js
+++ routers/athlete.router.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const AthleteController = require('../controllers/athlete.controller');
 
+const Athlete = require('../models/athlete');
+const Sport = require('../models/sport');
+const genderEnum = require("../models/gender");
+const countryEnum = require("../models/country");
+
 // on créé une nouvelle instance de notre controller !
 const athleteController = new AthleteController();
 // on spécifie le router express
@@ -14,6 +19,52 @@ router.get('/athletes', async (req, res) => {
 
     athleteController.getAll(req, res);
 })
+
+
+router.get('/api/athletes', async (req, res) => {
+    // on va devoir récupérer depuis la base de données nos sports
+    const athletes = await Athlete.find({});
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(athletes));
+})
+
+// create athlete api
+router.post('/api/athletes', async (req, res) => {
+    const paramAthlete = req.body;
+
+    const athlete = new Athlete({
+        firstName: paramAthlete.firstName,
+        lastName: paramAthlete.lastName,
+        gender: paramAthlete.gender,
+        country: paramAthlete.country
+    });
+    await athlete.save();
+
+    res.redirect('/athletes');
+})
+
+// get sports from athlete
+router.get('/api/athletes/:athleteId/sports', async (req, res) => {
+    const athleteId = req.params.athleteId;
+    var l = []; // liste des sports
+
+    // on va devoir récupérer depuis la base de données nos sports
+    const sports = await Sport.find({});
+    sportLoop: for (var i in sports) {
+        for (var j in sports[i].athletes) {
+            if (sports[i].athletes[j]._id == athleteId) {
+                l.push(sports[i]);
+                continue sportLoop;
+            }
+        }
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(l));
+})
+
+
 
 // ici pas de classe, on export directement l'objet route
 module.exports = router;
--- routers/sport.router.js
+++ routers/sport.router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const SportController = require('../controllers/sport.controller');
 
+const Athlete = require('../models/athlete');
+const Sport = require('../models/sport');
+
+
 // on créé une nouvelle instance de notre controller !
 const sportController = new SportController();
 // on spécifie le router express
@@ -13,6 +17,59 @@ router.get('/sports', async (req, res) => {
     res.render('sports', { sports });*/
 
     sportController.getAll(req, res);
+})
+
+router.get('/api/sports', async (req, res) => {
+    // on va devoir récupérer depuis la base de données nos sports
+    const sports = await Sport.find({});
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(sports));
+})
+router.post('/api/sports', async (req, res) => {
+    const paramSport = req.body;
+
+    const sport = new Sport({
+        name: paramSport.name,
+        athletes: []
+    });
+    await sport.save();
+
+    res.redirect('/sports');
+})
+
+router.get('/api/sports/:sportId/athletes', async (req, res) => {
+    const sportId = req.params.sportId;
+    var l = []; // liste des athletes
+
+    // on va devoir récupérer depuis la base de données nos sports
+    const sport = await Sport.findById(sportId);
+    for (var i in sport.athletes) { //
+        l.push( await Athlete.findById(sport.athletes[i]) );
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(l));
+})
+
+router.post('/api/sports/:sportId/athletes/:athleteId', async (req, res) => {
+    const sportId = req.params.sportId;
+    const athleteId = req.params.athleteId;
+
+    const sport = await Sport.findById(sportId);
+    if (sport.athletes.indexOf(athleteId) < 0) sport.athletes.push(athleteId)
+    await sport.save();
+
+    res.redirect('/sports');
+})
+
+router.post('/api/sports/delete/:sportId', async (req, res) => {
+    const sportId = req.params.sportId;
+
+    const sport = await Sport.deletOne(sportId);
+    //await sport.save();
+
+    res.redirect('/sports');
 })
 
 // ici pas de classe, on export directement l'objet route
